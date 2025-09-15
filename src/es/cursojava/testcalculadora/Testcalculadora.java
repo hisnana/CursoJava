d724d55b90a9a66e4f3aa188f99f072594a2ffa7
AnaArroyo <Tardes@GET-01>
2025-09-15 19:58:10 +0200

    pom.xml

---
+++ src/es/cursojava/testcalculadora/Testcalculadora.java
@@ -0,0 +1,15 @@
+package es.cursojava.testcalculadora;
+
+import es.cursojava.calculadora.Calculadora;
+
+public class Testcalculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculadora calculate = new Calculadora();
+		calculate.calcular("suma", 3, 3);
+
+	}
+
+}

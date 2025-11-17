c304141910dcd48b5b4a0237697c9c4052d86bff
AnaArroyo <Tardes@GET-01>
2025-11-17 20:00:54 +0100

    update plantilla JDBC

--- src/es/cursojava/plantillas/JDBCPlantilla.java
+++ src/es/cursojava/plantillas/JDBCPlantilla.java
@@ -11,6 +11,27 @@ public class JDBCPlantilla {
         String user = "HR";
         String pass = "hr";
         String sql = "SELECT first_name FROM employees WHERE employee_id = ?";
+//        //String sql = """
+//        	    CREATE TABLE TB_PRODUCTOS (
+//        	        ID      NUMBER(10),
+//        	        NOMBRE  VARCHAR2(100) NOT NULL,
+//        	        PRECIO  NUMBER(8,2) CHECK (PRECIO >= 0),
+//        	        CONSTRAINT PK_TB_PRODUCTOS PRIMARY KEY (ID)
+//        	    )
+//        	""";
+        
+        //Para la creación de la tabla
+//        try (Connection conn = obtenerConexion();
+//        	     Statement st = conn.createStatement()) {
+//
+//        	    st.executeUpdate(sql);
+//        	    conn.commit(); // si estás en transacción manual
+//
+//        	} catch (SQLException e) {
+//        	    conn.rollback(); // si procede
+//        	    e.printStackTrace();
+//        	}
+
 
         try (Connection con = DriverManager.getConnection(url, user, pass);
              PreparedStatement ps = con.prepareStatement(sql)) {
